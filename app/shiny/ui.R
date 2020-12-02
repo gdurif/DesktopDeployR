ebf795512faf1fd579433291cc991f03f9c2b921
GD <gd.dev@libertymail.net>
2020-12-02 10:06:18 +0100

    diyabcGUI app

--- app/shiny/ui.R
+++ app/shiny/ui.R
@@ -1,29 +1,3 @@
-#' This is an example shiny application
-#' It is the same as example_02 in the RStudio Shiny Tutorial
-
 library(shiny)
-
-ui = fluidPage(
-  titlePanel("Example Shiny App"),
-  sidebarLayout(
-
-    sidebarPanel(
-      selectInput(
-        inputId = "dataset",
-        label = "Choose a dataset",
-        choices = c("rock", "pressure", "cars", "iris")),
-
-      numericInput(
-        inputId = "obs",
-        label = "Number of observations to view:",
-        value = 10)
-    ),
-
-    mainPanel(
-      tags$div(sprintf("Global Variable Value: %s", GLOBAL_VAR)),
-      verbatimTextOutput("commandArgs"),
-      verbatimTextOutput("dataSummary"),
-      tableOutput("dataView")
-    )
-  )
-)
+library(diyabcGUI)
+ui <- diyabcGUI::diyabc_ui()
