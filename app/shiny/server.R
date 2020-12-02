3183f709c86a08d357d231d538267059c0127e88
GD <gd.dev@libertymail.net>
2020-12-02 10:06:18 +0100

    diyabcGUI app

--- app/shiny/server.R
+++ app/shiny/server.R
@@ -1,35 +1,5 @@
-#' This is an example Shiny application
-#' It is the same as example_02 in the RStudio Shiny Tutorial
-
 library(shiny)
-
-server = function(input, output, session) {
-
-  # IMPORTANT!
-  # this is needed to terminate the R process when the
-  # shiny app session ends. Otherwise, you end up with a zombie process
-  session$onSessionEnded(function() {
-    stopApp()
-  })
-
-  dataset = reactive({
-    switch(
-      input$dataset,
-      "rock" = rock,
-      "pressure" = pressure,
-      "cars" = cars,
-      "iris" = iris)
-  })
-
-  output$commandArgs = renderPrint({
-    commandArgs()
-  })
-
-  output$dataSummary = renderPrint({
-    summary(dataset())
-  })
-
-  output$dataView = renderTable({
-    head(dataset(), n = input$obs)
-  })
+server <- function(input, output, session) {
+    require(diyabcGUI)
+    diyabcGUI::diyabc_server(input, output, session)
 }
