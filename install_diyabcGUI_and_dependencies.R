7c921f66ec8db7e059b2c5a21176e20497312657
GD <gd.dev@libertymail.net>
2020-12-03 08:43:45 +0100

    fix installation of diyabcGUI + locall install of Rcpp because of incompatibility of compiled version for R 4.0.3 with R 4.0.0

--- install_diyabcGUI_and_dependencies.R
+++ install_diyabcGUI_and_dependencies.R
@@ -8,14 +8,21 @@ local({
     options(repos=r)
 })
 
+# R lib
+R_lib <- file.path("app", "library")
+if(dir.exists(R_lib)) dir.create(R_lib)
+
 # requirement
-install.package("devtools", lib = "app/library")
-install.packages("jsonlite", lib = "app/library")
-dep <- read.table("requirements.txt")
-install.packages(dep, lib = "app/library")
+install.package("devtools", lib = R_lib)
+install.packages("jsonlite", lib = R_lib)
+dep <- unlist(read.table("requirements.txt", header = FALSE, stringsAsFactors = FALSE))
+install.packages(dep, lib = R_lib)
+
+# local install from Rcpp
+devtools::install("Rcpp", args = c('--library="./app/library/"'))
 
 # install from zip source
-zip_src <- tail(sort(list.files(".", pattern = "diyabcGUI*")), 1)
+zip_src <- tail(sort(list.files("src", pattern = "diyabcGUI*")), 1)
 install.packages(
-    zip_src, repos = NULL, type = "win.binary", lib = "app/library"
+    file.path("src", zip_src), repos = NULL, type = "win.binary", lib = R_lib
 )
